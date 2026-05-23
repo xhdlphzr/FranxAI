5012d24c6bba477208a61c9c17f9fd552cd6af75
xhdlphzr <xhdlphzr@163.com>
2026-05-23 11:22:04 +0800

    feat: local model

---
+++ models/all-MiniLM-L12-v2/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a9c5a266121350036e45a733802ac6a8567604be365de01e40aa4b43e5ee594d
+size 10514
---
+++ models/all-MiniLM-L12-v2/train_script.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:176dd4e16a71a0e5cf76cefb0519fbc130e7ac3ee0b7ad707ef24067e6514be5
+size 13159
